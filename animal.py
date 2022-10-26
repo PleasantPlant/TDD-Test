980798ad66d51c9f0d021314b65608517dedf474
PleasantPlant <yoshihiro.m.shimokawa@gmail.com>
2022-10-26 11:53:55 -0400

    initial commit

---
+++ animal.py
@@ -0,0 +1,14 @@
+class Animal:
+    def __init__(self) -> None:
+        self.type = 0
+        self.size = 0
+        self.age = 0
+        self.name = 0
+
+    # I didn't put self in these because it wasn't included in the requirements of the assignment.
+    # Easy fix if I'm supposed to put them in.
+    def speak():
+        pass
+
+    def describe():
+        pass
