edeb1fa4dfa2f7881611ba61e86ec508dec72479
PleasantPlant <yoshihiro.m.shimokawa@gmail.com>
2022-10-27 12:59:11 -0400

    following class

--- animal.py
+++ animal.py
@@ -1,9 +1,9 @@
 class Animal:
     def __init__(self):
-        self.type = 0
-        self.size = 0
+        self.type = ""
+        self.size = ""
         self.age = 0
-        self.name = 0
+        self.name = ""
 
     # I didn't put self in these because it wasn't included in the requirements of the assignment.
     # Easy fix if I'm supposed to put them in.
