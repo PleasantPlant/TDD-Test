558008ff90ec48ea1469fcf74a4e8b8b4a5b676e
PleasantPlant <yoshihiro.m.shimokawa@gmail.com>
2022-11-03 10:35:51 -0400

    updating animal and test for animal

--- animal.py
+++ animal.py
@@ -1,14 +1,37 @@
 class Animal:
-    def __init__(self):
-        self.type = ""
+    def __init__(self, animalType=str):
+        self.type = animalType
         self.size = ""
         self.age = 0
         self.name = ""
+        match self.type.lower():
+            case "cat":
+                self.name = "Seymour"
+            case "dog":
+                self.name = "Spot"
 
-    # I didn't put self in these because it wasn't included in the requirements of the assignment.
-    # Easy fix if I'm supposed to put them in.
     def speak(self):
-        pass
+        match self.type.lower():
+            case "cat":
+                if self.size.lower() == "small":
+                    return "meow"
+                else:
+                    return "Meow!"
+            case "dog":
+                match self.size.lower():
+                    case "small":
+                        return "bow wow"
+                    case "medium":
+                        return "Ruff ruff"
+                    case "large":
+                        return "arf arf"
+                    case _:
+                        return "how did you set an invalid size?"
+            case _:
+                return "how did you set an invalid type?"
 
     def describe(self):
-        pass
+        if self.age <= 10:
+            return f"{self.name} is young"
+        elif self.age > 10:
+            return f"{self.name} is old"
